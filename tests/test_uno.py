5e71336dc6516486afd04bc4783e2b2a674b38e7
SuQichen777 <hdredbean123123123@outlook.com>
2025-03-14 18:41:46 -0400

    Refine README and function testcase

--- tests/test_uno.py
+++ tests/test_uno.py
@@ -1,8 +1,24 @@
 import pytest
-from terminalUno import Card, Color, Type, Deck, Player, is_valid_play
+from terminalUno import Card, Color, Type, Deck, Player, is_valid_play, uno
 # for now run "PYTHONPATH=src pytest tests/test_uno.py" to activate tests
 
 class TestUno:
+
+    def test_initialize_players(self):
+         players = uno.initialize_players(2, True)[0]
+         assert len(players) == 3, "the number of players when 2 players are initialized should be 2"
+         print("Initialize players test passed")
+
+    def test_initialize_deck(self):
+         players = uno.initialize_players(2, True)[0]
+         deck, discard_pile = uno.initialize_deck_and_discard_pile(cardNumMax = 10, initialCard = 7, players = players)
+         right_length = 108 - 21 - 1
+         assert len(deck) == right_length, "the number of cards in the deck should be 108"
+         print("Initialize deck test passed")
+         assert len(discard_pile) == 1, "the number of cards in the discard pile should be 1"
+         print("Initialize discard pile test1 : only 1 card in the discard_pile passed")
+         assert discard_pile[0] not in deck, "the discard pile should not be in the deck"
+         print("Initialize discard pile test2 : initial_discard not in deck passed")
 
     def test_sanity(self):
         expected = True
